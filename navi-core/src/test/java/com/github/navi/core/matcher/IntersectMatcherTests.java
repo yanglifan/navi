3a4ac152d3acfb9aad6098a050b2df849524d2ed
Yang Lifan <yanglifan@gmail.com>
2018-05-21 15:27:25 +0800

    Test intersect on collection

--- navi-core/src/test/java/com/github/navi/core/matcher/IntersectMatcherTests.java
+++ navi-core/src/test/java/com/github/navi/core/matcher/IntersectMatcherTests.java
@@ -5,7 +5,9 @@ import com.github.navi.core.SimpleSelector;
 import org.assertj.core.util.Sets;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -15,6 +17,19 @@ public class IntersectMatcherTests {
 	public void intersect_string() {
 		Map<String, String> request = new HashMap<>();
 		request.put("name", "stark, thor");
+
+		SimpleSelector simpleSelector = new SimpleSelector();
+		simpleSelector.registerCandidates(Handler.class, Sets.newLinkedHashSet(new IntersectHandler()));
+
+		Handler handler = simpleSelector.select(request, Handler.class);
+
+		assertThat(handler).isInstanceOf(IntersectHandler.class);
+	}
+
+	@Test
+	public void intersect_collection() {
+		Map<String, List<String>> request = new HashMap<>();
+		request.put("name", Arrays.asList("stark", "thor"));
 
 		SimpleSelector simpleSelector = new SimpleSelector();
 		simpleSelector.registerCandidates(Handler.class, Sets.newLinkedHashSet(new IntersectHandler()));
